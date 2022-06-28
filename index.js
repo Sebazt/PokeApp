5b78dbcbb666f9478671b9a3194068fb37133f58
Sebazt <juan.londono29@udea.edu.co>
2022-06-27 22:37:47 -0500

    add nodemon

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const bodyparser = require('body-parser')
+
+const server = express()
+
+// const PORT = process.env.PORT || 3000
+
+server.listen(3000,() => console.log('PokeServer runing on port 3000'))
